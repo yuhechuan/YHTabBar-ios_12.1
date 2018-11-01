7489d3c23c3f7aca2022cbf35cf1541bf89ff927
玉河川 <yuhechuan@ruaho.com>
2018-11-01 11:11:37 +0800

    首次提交添加文件

---
+++ UITabBar+ios12_1.h
@@ -0,0 +1,17 @@
+//
+//  UITabBar+ios12_1.h
+//  EaseChat
+//
+//  Created by apple on 2018/11/1.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITabBar (ios12_1)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UITabBar+ios12_1.m
@@ -0,0 +1,54 @@
+//
+//  UITabBar+ios12_1.m
+//  EaseChat
+//
+//  Created by apple on 2018/11/1.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "UITabBar+ios12_1.h"
+#import <objc/runtime.h>
+
+@implementation UITabBar (ios12_1)
+
++ (void)load {
+    /* 这个问题是 iOS 12.1 Beta 2 的问题，只要 UITabBar 是磨砂的，并且 push viewController 时 hidesBottomBarWhenPushed = YES 则手势返回的时候就会触发。
+     
+     出现这个现象的直接原因是 tabBar 内的按钮 UITabBarButton 被设置了错误的 frame，frame.size 变为 (0, 0) 导致的。如果12.1正式版Apple修复了这个bug可以移除调这段代码(来源于QMUIKit的处理方式)*/
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if (@available(iOS 12.1, *)) {
+            OverrideImplementation(NSClassFromString(@"UITabBarButton"), @selector(setFrame:), ^id(__unsafe_unretained Class originClass, SEL originCMD, IMP originIMP) {
+                return ^(UIView *selfObject, CGRect firstArgv) {
+                    
+                    if ([selfObject isKindOfClass:originClass]) {
+                        // 如果发现即将要设置一个 size 为空的 frame，则屏蔽掉本次设置
+                        if (!CGRectIsEmpty(selfObject.frame) && CGRectIsEmpty(firstArgv)) {
+                            return;
+                        }
+                    }
+                    
+                    // call super
+                    void (*originSelectorIMP)(id, SEL, CGRect);
+                    originSelectorIMP = (void (*)(id, SEL, CGRect))originIMP;
+                    originSelectorIMP(selfObject, originCMD, firstArgv);
+                };
+            });
+        }
+    });
+}
+
+CG_INLINE BOOL
+OverrideImplementation(Class targetClass, SEL targetSelector, id (^implementationBlock)(Class originClass, SEL originCMD, IMP originIMP)) {
+    Method originMethod = class_getInstanceMethod(targetClass, targetSelector);
+    if (!originMethod) {
+        return NO;
+    }
+    IMP originIMP = method_getImplementation(originMethod);
+    method_setImplementation(originMethod, imp_implementationWithBlock(implementationBlock(targetClass, targetSelector, originIMP)));
+    return YES;
+}
+
+
+
+@end
